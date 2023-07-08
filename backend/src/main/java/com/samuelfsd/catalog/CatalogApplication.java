1886d2cb64b295d3daa915b10836420fc737c8bc
samuelfsd <samuel.estudo778@gmail.com>
2023-07-08 15:17:11 -0300

    chore: add backend

---
+++ backend/src/main/java/com/samuelfsd/catalog/CatalogApplication.java
@@ -0,0 +1,13 @@
+package com.samuelfsd.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogApplication.class, args);
+	}
+
+}
