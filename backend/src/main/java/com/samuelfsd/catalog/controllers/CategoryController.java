3fa362ac9f14f7d4233c8bf6d847fa05dce3867d
samuelfsd <samuel.estudo778@gmail.com>
2023-07-09 16:46:40 -0300

    feat: create endpoint createCategory

--- backend/src/main/java/com/samuelfsd/catalog/controllers/CategoryController.java
+++ backend/src/main/java/com/samuelfsd/catalog/controllers/CategoryController.java
@@ -5,7 +5,9 @@ import com.samuelfsd.catalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,17 +17,28 @@ public class CategoryController {
 
     @Autowired
     private CategoryService service;
-    @GetMapping()
+    @GetMapping
     public ResponseEntity<List<CategoryDTO>> findAllCategories(){
         List<CategoryDTO> listCategories = service.findAllCategories();
 
         return ResponseEntity.ok().body(listCategories);
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<CategoryDTO> findCategoryById(@PathVariable Long id){
         CategoryDTO categoryDTO = service.findCategoryById(id);
 
         return ResponseEntity.ok().body(categoryDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO dto) {
+        dto = service.createCategory(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(dto);
     }
 }
